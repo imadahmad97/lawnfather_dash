53430041fe0ad673a5f800291f7c3b20ba1db7b4
Imad Ahmad <99471806+imadahmad97@users.noreply.github.com>
2023-08-30 21:05:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ external_stylesheets = [
     'https://codepen.io/chriddyp/pen/bWLwgP.css'
 ]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-application = app.server
+server = app.server
 app.title = "Sikandar Khattak's Dashboard"
 
 # Pie chart data
@@ -93,5 +93,5 @@ app.layout = html.Div(style={'display': 'flex'}, children=[
 ])
 
 # Run the app
-if __name__ == '__main__':
-    application.run(debug=True, port=8050)
+if __name__ == "__main__":
+    app.run_server(debug=False)
